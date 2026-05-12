64847d1a736a33b22b7edc1243c556c4dbed2b3b
amysmunson <amunson95070@gmail.com>
2026-05-12 15:13:53 -0700

    Adding home nav to footer

--- app/components/utils/Footer.js
+++ app/components/utils/Footer.js
@@ -7,6 +7,9 @@ export default function Footer() {
     return (
         <footer className="text-left p-4 text-sm text-white bg-blue-950">
             <div className="m-15 mb-10">
+                <Link href="/" onClick={() => setOpen(false)} className="block mb-5">
+                    Home
+                </Link>
                 <Link href="/properties" onClick={() => setOpen(false)} className="block mb-5">
                     Properties
                 </Link>
